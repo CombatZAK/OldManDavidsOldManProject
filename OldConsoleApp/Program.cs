e6428c871f8f94665775003545d2c9de318c741e
Zach Hess <zahess@microsoft.com>
2018-11-30 10:02:05 -0500

    FIRSTgit add .1

---
+++ OldConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Old man David says, \"Get off my lawn!\"");
+        }
+    }
+}
